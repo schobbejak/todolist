387c75e703fc1aafa4316bef235ddc7a0ddb86fb
jvanselm <jmvanselm@gmail.com>
2023-11-21 00:03:51 +0100

    Working database with postgresql

--- todolist_backend/src/main/java/todolist/todolist_backend/entities/Todo.java
+++ todolist_backend/src/main/java/todolist/todolist_backend/entities/Todo.java
@@ -6,20 +6,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
+@Table(name = "todos")
 public class Todo {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "date")
     private String date;
 
+    @Column(name = "completed")
     private boolean completed;
 
     public Todo() {   
